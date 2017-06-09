f935294d05d43fe16e7f314063e2fa332f4a192f
Łukasz Borowy <lucas.borowy@gmail.com>
2017-06-09 21:01:27 +0200

    Initial commit

---
+++ app/src/main/java/pl/lborowy/layouts/MainActivity.java
@@ -0,0 +1,23 @@
+package pl.lborowy.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_layout);
+    }
+
+
+
+    public void sendMessage(View view) {
+
+    }
+}
