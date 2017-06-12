81184f4ab5dd15200d73d834295bd164380cfaf2
Łukasz Borowy <lucas.borowy@gmail.com>
2017-06-12 20:13:01 +0200

    v1

--- app/src/main/java/pl/lborowy/layouts/MainActivity.java
+++ app/src/main/java/pl/lborowy/layouts/MainActivity.java
@@ -2,22 +2,53 @@ package pl.lborowy.layouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-
+    private EditText messageEditText;
+    private ImageView sendImage;
+    private LinearLayout messagesLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_layout);
+
+        messageEditText = (EditText) findViewById(R.id.messageEditText);
+        sendImage = (ImageView) findViewById(R.id.sendMessage);
+        messagesLinearLayout = (LinearLayout) findViewById(R.id.mainChat);
+
+        sendImage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        sendMessage(v);
     }
 
 
-
     public void sendMessage(View view) {
+        String message = messageEditText.getText().toString();
+        messageEditText.setText(null);
 
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+        LayoutInflater inflater = getLayoutInflater();
+
+        inflater.inflate(R.layout.my_message_item, messagesLinearLayout, true);
+        View messageView = messagesLinearLayout.getChildAt(messagesLinearLayout.getChildCount() - 1);
+        if(messageView instanceof TextView)
+            ((TextView) messageView).setText(message);
+
+        TextView textView = (TextView) inflater.inflate(R.layout.my_message_item, messagesLinearLayout, false);
+        textView.setText(message);
+        messagesLinearLayout.addView(textView);
     }
 }
---
+++ app/src/main/java/pl/lborowy/layouts/TableActivity.java
@@ -0,0 +1,13 @@
+package pl.lborowy.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.table_layout_task1);
+    }
+}
